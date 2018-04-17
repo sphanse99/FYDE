dfdfc89bb83e9e54cb58425375a42b3f7b8575c0
Vivek Malneedi <vivekmalneedi@gmail.com>
2018-04-17 15:05:26 -0500

    initial files

---
+++ FYDE_Final/FYDE_Final.ino
@@ -0,0 +1,64 @@
+//www.elegoo.com
+//2016.06.13
+
+#define STEP_NUMBER 1
+
+#include <Wire.h>
+#include "rgb_lcd.h"
+
+int Led = 13;//define LED port
+int speaker = 7;
+int light = 8;
+int sensorPin = 3; //define switch port
+int val;//define digital variable val
+int check = 0;
+
+rgb_lcd lcd;
+
+ int colorR = 0;
+ int colorG = 255;
+ int colorB = 0;
+
+void setup(){
+  pinMode(Led,OUTPUT);//define LED as a output port
+  pinMode(sensorPin,INPUT);//define switch as a output port
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.setRGB(colorR, colorG, colorB);
+    lcd.print("You Gucci");
+
+  pinMode(13, OUTPUT);
+}
+
+void loop(){ 
+  val=digitalRead(sensorPin);//read the value of the digital interface 3 assigned to val 
+  
+  Serial.println(val);
+  
+  if(val==HIGH && check == 0){
+    digitalWrite(speaker,HIGH);
+    digitalWrite(light,HIGH);
+    colorR = 255;
+    colorG = 0;
+    colorB = 0;
+    lcd.setCursor(0, 0);
+    lcd.setRGB(colorR, colorG, colorB);
+    lcd.print("FUCK OFF, THIEF!");
+    check = 1;
+  }else if(val == HIGH && check == 1){
+  }else if(val == LOW && check == 1){
+    digitalWrite(speaker,LOW);
+    digitalWrite(light, LOW);
+
+    colorR = 0;
+    colorG = 255;
+    colorB = 0;
+    lcd.setCursor(0, 0);
+    lcd.setRGB(colorR, colorG, colorB);
+    lcd.print("You Gucci       ");
+    check = 0;
+  }else if(val == LOW && check == 0){
+  }
+  Serial.println(check);
+}
+
